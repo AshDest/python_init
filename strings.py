98f249fa50cdd111b37d012c03be564b59e1cf95
unknown <ashuzamaheshe4@gmail.com>
2022-09-25 12:29:19 +0200

    ash commit

--- strings.py
+++ strings.py
@@ -1,5 +1,18 @@
-print("bonjour".upper())
-print("BONJOUR".lower())
-print("bonjour lundi de mardi".capitalize())
-print("bonjour lundi de mardi".title())
-print("\u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764")
+print("bonjour".upper()) #majuscule
+print("BONJOUR".lower()) #miniscule
+print("bonjour lundi de mardi".capitalize()) # Premiere lettre en Majuscule
+print("bonjour lundi de mardi".title()) #Premiere lettre de chaque mot en majuscule
+print("\u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764") #love love
+
+print("Bonjour".replace('jour', "soir")) #pour remplacer un caractere dans une chaine
+
+print(" bonjour Maman de Maman".strip())
+
+print("1, 2, 3, 4, 5".split(", "))
+
+print(".".join("1, 2, 3, 4, 5".split(", ")))
+
+for i in range(10):
+    print(str(i).zfill(3))
+
+print("\u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764 \u2764") #love love
