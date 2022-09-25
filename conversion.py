4a10c9c5f2d08e2de531caf0651570c5621c5850
unknown <ashuzamaheshe4@gmail.com>
2022-09-25 10:46:35 +0200

    ash commit

--- conversion.py
+++ conversion.py
@@ -1,4 +1,9 @@
-a = "J'ai une classe de " + 30 + "Eleves"
-b = 10 + " + " + int("5") + " est egal a " + 15
-c = 10 + "5"
-d = "L'addition de 10 + 5 est egal a " + 10 + 5
+a = "J'ai une classe de " + str(30) + " Eleves"
+b = str(10) + " + " + "5" + " est egal a " + str(15)
+c = str(10) + "5"
+d = "L'addition de 10 + 5 est egal a " + str(10 + 5)
+
+print(a)
+print(b)
+print(c)
+print(d)
