1b135712e7a3e41cc845ec112fa81d499284b27f
unknown <ashuzamaheshe4@gmail.com>
2022-09-26 11:42:14 +0200

    ash commit

---
+++ structure_2.py
@@ -0,0 +1,2 @@
+a = int(input("Entrer votre Age actuel : "))
+print("Vous etes Majeur") if a >= 18 else print("Vous etes Mineur")
--- structures_conditionnelles.py
+++ structures_conditionnelles.py
@@ -1,6 +1,8 @@
 age = int(input("Entrer votre age : "))
-if age >= 18:
+if age > 18:
     print("Vous etes Majeur \u2764 \u2764")
-elif age < 18:
-    print("Vous etes Mineur!")
+elif age == 18:
+    print("Vous nouvellement majeur \u2764")
+else:
+    print("Vous etes Mineur")
     
