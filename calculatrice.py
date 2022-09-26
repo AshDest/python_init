9b585a4fca896d40c5a0acc9eaf92f2f13c17676
unknown <ashuzamaheshe4@gmail.com>
2022-09-26 11:10:36 +0200

    ash commit

--- calculatrice.py
+++ calculatrice.py
@@ -1,4 +1,9 @@
+from unittest import result
+
+
 nbre_1 = input("Entrer le premier nombre: ")
 nbre_2 = input("Entrer le deuxieme nombre: ")
-resultat = int(nbre_1 + nbre_2)
+int(nbre_1)
+int(nbre_2)
+resultat = nbre_1 + nbre_2
 print(f"Le resultat de l'addition de {nbre_1} avec {nbre_2} est " + str(resultat))
---
+++ structures_conditionnelles.py
@@ -0,0 +1,6 @@
+age = int(input("Entrer votre age : "))
+if age >= 18:
+    print("Vous etes Majeur \u2764 \u2764")
+elif age < 18:
+    print("Vous etes Mineur!")
+    
