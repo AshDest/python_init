66152702b6ce28c37019ee379103ebba5488702c
unknown <ashuzamaheshe4@gmail.com>
2022-09-27 18:27:46 +0200

    ash commit

--- list_tranche.py
+++ list_tranche.py
@@ -1,7 +1,11 @@
+#ulisation des tranches avec Des listes
+
 liste = ["user_1",
          "user_2",
          "user_3",
          "user_4"]
-print(liste[0:3])
-print(liste[-4:-1])
-print(liste[:2])
+print(liste[0:3]) # A partir de l'index 0 de la liste, jusqu' a l'index 3 de la liste
+print(liste[-4:-1]) # A partir de l'index -4 jusqu'a l'index -1 de la liste
+print(liste[:2]) # A partir du debut, jusqu'a l'index 2 de la liste
+print(liste[2:]) # A partir de l'index 2, jusqu'a la fin de la liste
+print(liste[::2]) #utilisation des pas dans une liste
