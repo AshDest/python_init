ccd5b2b75a6b3fe1e6d2afa600f9f0b4019b9f82
unknown <ashuzamaheshe4@gmail.com>
2022-09-27 14:55:28 +0200

    ash commit

---
+++ list.py
@@ -0,0 +1,9 @@
+liste = [1, 2, 3, 4, 5, 6]
+liste.extend([1, 4, 9, 0])
+print(liste)
+
+liste.remove(5)
+print(liste)
+
+print(liste[5] + liste[0])
+print(liste[-2])
--- module_os.py
+++ module_os.py
@@ -3,5 +3,5 @@ import os
 chemin ="C:/Users/hp PC/Documents/Python"
 dossier = os.path.join(chemin, "dossier", "test")
 if not os.path.exists(dossier):
-    os.makedirs(dossier)
+    os.removedirs(dossier)
 os.makedirs(dossier, exist_ok=True)
