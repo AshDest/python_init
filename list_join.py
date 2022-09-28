97e956f452e1569df60527ec135229fd0dbb5418
unknown <ashuzamaheshe4@gmail.com>
2022-09-28 14:10:35 +0200

    ash commit

---
+++ list_join.py
@@ -0,0 +1,4 @@
+liste = ["python", "est", "un", "langage", "incroyable"]
+resultat = " ".join(liste) # Ajoute un espace apres chaque element de la liste
+
+print(resultat)
--- list_methode.py
+++ list_methode.py
@@ -1,8 +1,5 @@
 # utilisation des methodes dans une liste
 
-from unittest import result
-
-
 employes = ["Carlos", "Max", "Martine", "Patrick", "Alex"]
 
 resultat = employes.index("Max") #pour avoir l'index dans la liste, la position
@@ -12,3 +9,11 @@ print(resultat) # le nombre de fois qu'une valeur se repete dans la liste
 
 employes.sort() # pour groupper les contenu de la liste soit selon lordre croissant
 print(employes)
+
+employes.reverse() # Pour inverser l'ordre dans une liste
+print(employes)
+
+employes.pop(-1) # pour enlever un element de la liste a partir de son index
+print(employes)
+
+employes.clear() # pour supprimer tous les elements des la liste
