55ffa9417a8c6bc4216359a75332cfe4fc814c08
unknown <ashuzamaheshe4@gmail.com>
2022-09-29 18:33:02 +0200

    exercices

--- boucle_while.py
+++ boucle_while.py
@@ -1,4 +1,15 @@
+import time
+
 i = 0
 while i < 10:
     print("Bonjour")
     i += 1
+
+continuer = "o"
+while continuer == "o":
+    print("On continue...")
+    continuer = input("Voulez-vous continuer? o/n ")
+
+while True:
+    print("Sauvegarde en cours...")
+    time.sleep(600)
---
+++ continue_break.py
@@ -0,0 +1,5 @@
+liste = ["1", "4", "25", "Paul", "3", "Pierre"]
+for i in liste:
+    if i.isdigit():
+        continue
+    print(i)
