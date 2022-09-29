7788904175a5087912cbdb72eee3a7f7463f15e3
unknown <ashuzamaheshe4@gmail.com>
2022-09-29 11:23:17 +0200

    ash commit

---
+++ functions_methods.py
@@ -0,0 +1,4 @@
+# Function sorted()
+liste = [5, 3, 9, 7, 1]
+liste = sorted(liste)
+print(liste)
---
+++ list_imbriquees.py
@@ -0,0 +1,2 @@
+liste = ["Python", ["Java", "C++", ["C"]], ["Ruby"]]
+print(liste[1][1]) # Pour naviger dans les indexs des ces tableaux
