033958ea5969b9316597b99fa7500352d4a7076a
unknown <ashuzamaheshe4@gmail.com>
2022-09-29 10:39:23 +0200

    ash commit

---
+++ operateurs_list.py
@@ -0,0 +1,6 @@
+utilisateurs = ["Paul", "Pierre", "Marie"]
+"Paul" in utilisateurs
+# TRUE
+
+"Java" in "Javascript"
+# TRUE
--- split_list.py
+++ split_list.py
@@ -1,3 +1,4 @@
 courses = "Riz, Pommes, Lait, Salade, Saumon, Beurre"
-courses = courses.split()
+courses_liste = courses.split(", ") # par defaut cette fonction utilise les espaces pour separer. mais on peut utiliser des caracters (",")
 print(courses)
+print(courses_liste)
