97813e602174ea4d26cfa9abfafe3d26d4d03a24
unknown <ashuzamaheshe4@gmail.com>
2022-09-30 13:45:16 +0200

    exercices

--- exercice_list.py
+++ exercice_list.py
@@ -26,3 +26,15 @@ print(nombres_paires)
 nombres = range(-10,10)
 nombres_positifs = [i for i in nombres if i > 0 ]
 print(nombres_positifs)
+
+# QUESTION 3 :
+# nombres = range(5)
+# nombres_doubles = []
+# for i in nombres :
+#     nombres_doubles.append(i * 2)
+# print(nombres_doubles)
+# RESOLUTION AVEC LIST COMPREHENSION
+# ==================================
+nombres = range(5)
+nombres_doubles = [i * 2 for i in nombres]
+print(nombres_doubles)
