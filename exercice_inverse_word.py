02e74b2f03f7481858d52d3c47d7acc188cd184e
unknown <ashuzamaheshe4@gmail.com>
2022-10-02 13:33:58 +0200

    exercices

---
+++ exercice_inverse_word.py
@@ -0,0 +1,13 @@
+from tkinter import Y
+
+
+mot = "Python"
+for i in reversed(mot):
+    print(i)
+    
+# P
+# y
+# t
+# h
+# o
+# n
--- exercice_utilisateur.py
+++ exercice_utilisateur.py
@@ -1,2 +1,11 @@
 for i in range(10):
     print("Utilisateur "+str(i + 1))
+    
+for i in range(1, 11) :
+    print(f"Utilisateur {i}")
+
+# Utilisateur 1
+# Utilisateur 2
+# Utilisateur 3
+
+# Utilisateur 10
