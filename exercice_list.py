00dc5167021372653fe1863f12a64c3bca564fbc
unknown <ashuzamaheshe4@gmail.com>
2022-10-02 13:18:32 +0200

    exercices

--- exercice_list.py
+++ exercice_list.py
@@ -1,4 +1,5 @@
 # QUESTION 1 : 
+
 # nombres = [1, 21, 5, 44, 4, 9, 5, 83, 29, 31, 25, 38]
 # nombres_paires = []
 # for i in nombres:
@@ -28,6 +29,7 @@ nombres_positifs = [i for i in nombres if i > 0 ]
 print(nombres_positifs)
 
 # QUESTION 3 :
+
 # nombres = range(5)
 # nombres_doubles = []
 # for i in nombres :
@@ -38,3 +40,17 @@ print(nombres_positifs)
 nombres = range(5)
 nombres_doubles = [i * 2 for i in nombres]
 print(nombres_doubles)
+
+# nombres = range(10)
+# nombre_inverses = []
+# for i in nombres:
+#     if i % 2 == 0:
+#         nombre_inverses.append(i)
+#     else:
+#         nombre_inverses.append(-i)
+# print(nombre_inverses)
+
+
+nombres = range(10)
+nombre_inverses = [i if i % 2 == 0 else -i for i in nombres ]
+print(nombre_inverses)
